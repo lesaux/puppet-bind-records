688054a57b2b4021d06ce9f4d92a3c71642ac49c
Pierig Le Saux <lesaux@pythian.com>
2014-08-11 16:44:51 -0400

    fixed parameterised class parameter without a default value on line 11

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,15 @@
-class bindrecords ($records) {
+# == Class: bindrecords
+#
+# A Puppet module to update dns records on an existing dns server
+#
+# === Parameters
+#
+# [*records*]
+#   a hash of dns records you wish to add
+#
+
+class bindrecords (
+  $records = undef,
+) {
 create_resources(dns_rr, $records)
 }
